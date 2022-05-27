4293c3674e93a8268ec2fa9791b65649b93fb411
guadarecio <97972523+guadarecio@users.noreply.github.com>
2022-05-27 13:05:50 -0300

    Adding a picker for different options

--- components/Formulario.js
+++ components/Formulario.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { StyleSheet, Text, View, ActivityIndicator } from "react-native";
 import { useFonts, Inter_900Black } from "@expo-google-fonts/inter";
+import { Picker } from "@react-native-picker/picker";
 
 export default function Formulario() {
   let [fontsLoaded] = useFonts({
@@ -19,6 +20,15 @@ export default function Formulario() {
   return (
     <View>
       <Text style={styles.label}>Moneda</Text>
+
+      <Picker>
+        <Picker.Item label="-Seleccione-" value="" />
+        <Picker.Item label="Dolar EE.UU" value="USD" />
+        <Picker.Item label="Peso Mexicano" value="MXN" />
+        <Picker.Item label="Euro" value="EUR" />
+        <Picker.Item label="Libra Esterlina" value="GBP" />
+      </Picker>
+
       <Text style={styles.label}>Criptomoneda</Text>
     </View>
   );

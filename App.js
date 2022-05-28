551424ae27d6cdf5c96bfd0ddc621b25c6e9022c
guadarecio <97972523+guadarecio@users.noreply.github.com>
2022-05-28 14:35:43 -0300

    Showing a Spinner in which you get a quote 5 minutes

--- App.js
+++ App.js
@@ -1,5 +1,12 @@
 import React, { useState, useEffect } from "react";
-import { StyleSheet, Text, View, Image, ScrollView } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  ScrollView,
+  ActivityIndicator,
+} from "react-native";
 import Header from "./components/Header";
 import Formulario from "./components/Formulario";
 import Cotizacion from "./components/Cotizacion";
@@ -10,6 +17,7 @@ const App = () => {
   const [criptomoneda, guardarCriptoMoneda] = useState("");
   const [consultarAPI, guardarConsultarAPI] = useState(false);
   const [resultado, guardarResultado] = useState({});
+  const [cargando, guardarCargando] = useState(false);
 
   useEffect(() => {
     const cotizarCriptomoneda = async () => {
@@ -17,15 +25,25 @@ const App = () => {
         const url = `https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${criptomoneda}&tsyms=${moneda}`;
         const resultado = await axios.get(url);
 
-        console.log(resultado.data.DISPLAY[criptomoneda][moneda]);
+        // console.log(resultado.data.DISPLAY[criptomoneda][moneda]);
+        guardarCargando(true);
 
-        guardarResultado(resultado.data.DISPLAY[criptomoneda][moneda]);
+        setTimeout(() => {
+          guardarResultado(resultado.data.DISPLAY[criptomoneda][moneda]);
 
-        guardarConsultarAPI(false);
+          guardarConsultarAPI(false);
+          guardarCargando(false);
+        }, 3000);
       }
     };
     cotizarCriptomoneda();
   }, [consultarAPI]);
+
+  const componente = cargando ? (
+    <ActivityIndicator size="large" color="#5E49E2" />
+  ) : (
+    <Cotizacion resultado={resultado} />
+  );
 
   return (
     <>
@@ -45,6 +63,8 @@ const App = () => {
             guardarConsultarAPI={guardarConsultarAPI}
           />
         </View>
+
+        <View style={{ marginTop: 40 }}>{componente}</View>
 
         <Cotizacion resultado={resultado} />
       </ScrollView>
